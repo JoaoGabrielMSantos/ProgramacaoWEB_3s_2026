1883454fca8c46a7dbe978fefea72c1b0657e646
JoaoGabrielMSantos <seu_email@example.com>
2026-03-17 09:44:55 -0300

    Adiciona lógica para o jogo Pedra, Papel e Tesoura e implementa a tabuada

---
+++ Atividade_JS/projeto/js/jogoPedraPapelTesoura.js
@@ -0,0 +1,20 @@
+let usuario = prompt("Escolha: pedra, papel ou tesoura").toLowerCase();
+
+let opcoes = ["pedra", "papel", "tesoura"];
+let computador = opcoes[Math.floor(Math.random() * 3)];
+
+console.log("Computador escolheu: " + computador);
+
+if (usuario === computador) {
+    console.log("Empate!");
+} 
+else if (
+    (usuario === "pedra" && computador === "tesoura") ||
+    (usuario === "papel" && computador === "pedra") ||
+    (usuario === "tesoura" && computador === "papel")
+) {
+    console.log("Você venceu!");
+} 
+else {
+    console.log("Computador venceu!");
+}
